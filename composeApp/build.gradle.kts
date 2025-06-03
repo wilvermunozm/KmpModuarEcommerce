c8e0e8bd499040727a01b974f312119130a6829a
Wilver Muñoz <wilver@mawiapps.com.co>
2025-06-03 09:48:30 -0500

    (FEAT): Android splash screen configuration

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -33,6 +33,7 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+            implementation(libs.splash.screen)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
